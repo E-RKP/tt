2872c6e9118eefd64ad5655713d3ac54bdfaa4d8
E-RKP <edobackup111@gmail.com>
2025-07-14 12:24:53 +0200

    version 2

--- data.js
+++ data.js
@@ -1,2 +1,15 @@
 let ciao = 'ciao'
 console.log(ciao)
+
+fetch("https://jsonplaceholder.typicode.com/users").then(function(placeholder){
+  return placeholder.json();
+}).then(function(placeholder){
+  for (i=0; i<placeholder.length;i++){
+    let ciao = placeholder[i].company.name
+    if (ciao.includes('Group')){
+      console.log(ciao)
+    }
+  }
+}).catch(function(error){
+  console.log(error)
+})
